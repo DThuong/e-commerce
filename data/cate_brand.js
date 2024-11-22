54ab4e5bb45510b8c27a3b86fb0162e43c4922d5
Danh Thuong <hachimansuvivel@gmail.com>
2024-11-22 13:48:08 +0700

    update scraping data for project

---
+++ data/cate_brand.js
@@ -0,0 +1,33 @@
+const data = [
+  {
+    cate: "Smartphone",
+    brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+  },
+  {
+    cate: "Tablet",
+    brand: ["Apple", "Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+  },
+  {
+    cate: "Laptop",
+    brand: ["Macbook", "Dell", "Acer", "Asus", "Lenovo", "Hp"],
+  },
+  {
+    cate: "Accessories",
+    brand: [
+      "Headphone",
+      "Bluetooth",
+      "Keyboard",
+      "Smartwatches",
+      "Mouse",
+      "Cases",
+    ],
+  },
+  {
+    cate: "Television",
+    brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+  },
+  {
+    cate: "Printer",
+    brand: ["Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+  },
+];

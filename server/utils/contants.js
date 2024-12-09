814871504d5dd99f5770046fcf0163bf53a816fb
Danh Thuong <hachimansuvivel@gmail.com>
2024-12-09 12:11:54 +0700

    update insert account user and admin

--- server/utils/contants.js
+++ server/utils/contants.js
@@ -1,46 +1,54 @@
 const users = [
-    {
-      email: 'user-01@gmail.com',
-      password: '123456',
-      firstname: 'user',
-      lastname: '01',
-      mobile: '098757653',
-    },
-    {
-      email: 'randomuser1@gmail.com',
-      password: 'abcdef12',
-      firstname: 'John',
-      lastname: 'Doe',
-      mobile: '091234567',
-    },
-    {
-      email: 'randomuser2@yahoo.com',
-      password: 'qwerty34',
-      firstname: 'Jane',
-      lastname: 'Smith',
-      mobile: '098765432',
-    },
-    {
-      email: 'randomuser3@hotmail.com',
-      password: 'zxcvbn56',
-      firstname: 'Alice',
-      lastname: 'Johnson',
-      mobile: '090123456',
-    },
-    {
-      email: 'randomuser4@outlook.com',
-      password: 'password78',
-      firstname: 'Robert',
-      lastname: 'Brown',
-      mobile: '092345678',
-    },
-    {
-      email: 'randomuser5@gmail.com',
-      password: 'hunter99',
-      firstname: 'Emily',
-      lastname: 'Clark',
-      mobile: '094567890',
-    },
-  ];
+  {
+    email: "admin@gmail.com",
+    password: "123456",
+    firstname: "admin",
+    lastname: "quản trị viên",
+    mobile: "61987649817234",
+    role: 2002,
+  },
+  {
+    email: "user-01@gmail.com",
+    password: "123456",
+    firstname: "user",
+    lastname: "01",
+    mobile: "098757653",
+  },
+  {
+    email: "randomuser1@gmail.com",
+    password: "abcdef12",
+    firstname: "John",
+    lastname: "Doe",
+    mobile: "091234567",
+  },
+  {
+    email: "randomuser2@yahoo.com",
+    password: "qwerty34",
+    firstname: "Jane",
+    lastname: "Smith",
+    mobile: "098765432",
+  },
+  {
+    email: "randomuser3@hotmail.com",
+    password: "zxcvbn56",
+    firstname: "Alice",
+    lastname: "Johnson",
+    mobile: "090123456",
+  },
+  {
+    email: "randomuser4@outlook.com",
+    password: "password78",
+    firstname: "Robert",
+    lastname: "Brown",
+    mobile: "092345678",
+  },
+  {
+    email: "randomuser5@gmail.com",
+    password: "hunter99",
+    firstname: "Emily",
+    lastname: "Clark",
+    mobile: "094567890",
+  },
+];
 
-module.exports = users
+module.exports = users;
